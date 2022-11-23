e39184a495092926039578682c31c4a362485785
theropoy <maximusjanz100@gmail.com>
2022-11-24 00:28:15 +0100

    hero edited

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,78 +1,89 @@
-import React from 'react'
-import styles from "../../styles/home/Hero.module.css"
-import profileImg from "../../assets/home/profile.png"
-import pbImg from "../../assets/home/pb.png"
-import helloImg from "../../assets/home/hello.svg"
-import avatarImg from "../../assets/home/avatar.svg"
-import profileMobileImg from "../../assets/home/profileMobile.png"
-import arrowImg from "../../assets/home/down-arrow-white.png"
-import { title } from 'process'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import styles from "../../styles/home/Hero.module.css";
+import pbImg from "../../assets/home/pb.png";
+import { useNavigate } from "react-router-dom";
 
-interface HeroProps {
-    aboutRef: React.MutableRefObject<HTMLDivElement | null>,
-    skillsRef: React.MutableRefObject<HTMLDivElement | null>,
-    portfolioRef: React.MutableRefObject<HTMLDivElement | null>,
-    contactRef: React.MutableRefObject<HTMLDivElement | null>,
-    ger: boolean
-
+interface Props {
+	aboutRef: React.MutableRefObject<HTMLDivElement | null>;
+	skillsRef: React.MutableRefObject<HTMLDivElement | null>;
+	portfolioRef: React.MutableRefObject<HTMLDivElement | null>;
+	contactRef: React.MutableRefObject<HTMLDivElement | null>;
+	ger: boolean;
 }
 
-const Hero: React.FC<HeroProps> = ({aboutRef, skillsRef, portfolioRef , contactRef, ger}) => {
+// Part of home
+// Hero element
+const Hero: React.FC<Props> = ({ aboutRef, skillsRef, portfolioRef, contactRef, ger }) => {
+	const navigate = useNavigate();
 
-    const navigate = useNavigate();
+	//List of Buttons with Name and the ref
+	const listOfButtons = [
+		{ txt: ger ? "Über Mich" : "About Me", ref: aboutRef },
+		{ txt: ger ? "Fähigkeit" : "Skills", ref: skillsRef },
+		{ txt: ger ? "Portfolio" : "Portfolio", ref: portfolioRef },
+	];
 
-    //List of Buttons with Name and the ref
-    const listOfButtons = [
-        {txt: ger? "Über Mich" : "About Me", ref: aboutRef},
-        {txt: ger? "Fähigkeit" : "Skills", ref: skillsRef},
-        {txt: ger? "Portfolio" : "Portfolio", ref: portfolioRef},
-    ]
+	//Scrolls to defined html element
+	const executeScroll = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
+		if (ref.current !== null) {
+			ref.current.scrollIntoView()!;
+		}
+	};
 
-    //Scrolls to defined html element
-    const executeScroll = (ref: React.MutableRefObject<HTMLDivElement | null>) => {
-        if(ref.current !== null) {
-            ref.current.scrollIntoView()!;
-        }     
-    }
+	const text = {
+		title: {
+			ger: (
+				<>
+					Hi,
+					<br />
+					Ich bin Maxim,
+					<br />
+					ein Entwickler.
+				</>
+			),
+			eng: (
+				<>
+					Hi,
+					<br />
+					I'm Maxim,
+					<br />a developer.
+				</>
+			),
+		},
+		contact: { ger: "Kontakt", eng: "Contact" },
+	};
 
-    const text = {
-        title: {ger: <>Hi,<br />Ich bin Maxim,<br />ein Entwickler.</>, eng: <>Hi,<br />I'm Maxim,<br />a developer.</>},
-        contact: {ger: "Kontakt", eng: "Contact"},
-    }
+	return (
+		<section className={styles.container}>
+			<div className={styles.langSwitch} onClick={() => (ger ? navigate("/eng") : navigate("/"))}>
+				<div className={ger ? styles.activee : ""}>de</div>
+				<div className={!ger ? styles.activee : ""}>eng</div>
+			</div>
+			<div className={styles.main}>
+				<div className={styles.pbContainer}>
+					<img className={styles.pb} src={pbImg} />
+				</div>
+				<div className={styles.content}>
+					<h2 className={styles.title}>{ger ? text.title.ger : text.title.eng}</h2>
+					<p className={styles.lowerTitle}>Front End, Wordpress, Mobile</p>
+				</div>
+			</div>
+			<div className={styles.contactContainer}>
+				<button onClick={(e) => executeScroll(contactRef)} className={styles.contact}>
+					{ger ? text.contact.ger : text.contact.eng}
+				</button>
+			</div>
+			<ul className={styles.nav}>
+				{listOfButtons.map((item, key) => {
+					return (
+						<li key={key} onClick={() => executeScroll(item.ref)}>
+							{"< " + item.txt + " />"}
+						</li>
+					);
+				})}
+			</ul>
+		</section>
+	);
+};
 
-
-    return (
-        <section className={styles.container}>
-            <div className={styles.langSwitch} onClick={() => ger? navigate("/eng") : navigate("/")}>
-                <div className={ger? styles.activee : ""}>de</div>
-                <div className={!ger? styles.activee : ""}>eng</div>
-            </div>
-            <div className={styles.main}>
-                <div className={styles.pbContainer}>
-                    <img className={styles.pb} src={pbImg}/>
-                </div>
-                <div className={styles.content}>
-                    <h2 className={styles.title}>{ger? text.title.ger : text.title.eng}</h2>
-                    <p className={styles.lowerTitle}>Front End, Wordpress, Mobile</p>
-                </div>
-            </div>
-            <div className={styles.contactContainer}>
-                <button onClick={(e) => executeScroll(contactRef)} className={styles.contact}>{ger? text.contact.ger : text.contact.eng}</button>
-            </div>
-            <ul className={styles.nav}>
-                {
-                    listOfButtons.map((item, key) => {
-                        return (
-                            <li key={key} onClick={() => executeScroll(item.ref)}>
-                                {"< " + item.txt + " />"}
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </section>
-    )
-}
-
-export default Hero
+export default Hero;
